eeea25bb270df10c4b6af4dc581832fcb82ee599
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-03 16:54:54 -0500

    WIP: UI panel to show score, coin score and modifier score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,38 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;public class GameManager : MonoBehaviour {  public static GameManager Instance { set; get; }  private bool isGameStarted = false;  private PenguinMotor motor;  public Text scoreText, coinText, modifierText;  private float score, coinScore, modifierScore;  private void Awake()  {
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+  public static GameManager Instance { set; get; }
+
+  private bool isGameStarted = false;
+  private PenguinMotor motor;
+
+  public Text scoreText, coinText, modifierText;
+  private float score, coinScore, modifierScore;
+
+  private void Awake()
+  {
         Instance = this;
-        //UpdateScores();
-        motor = GameObject.FindGameObjectWithTag("Player").GetComponent<PenguinMotor>();  }  private void Update()  {
+        UpdateScores();
+        motor = GameObject.FindGameObjectWithTag("Player").GetComponent<PenguinMotor>();
+  }
+
+  private void Update()
+  {
         if (MobileInput.Instance.Tap && !isGameStarted)
         {
             isGameStarted = true;
             motor.StartRunning();
-        }  }  public void UpdateScores()  {
+        }
+  }
+
+  public void UpdateScores()
+  {
         scoreText.text = score.ToString();
         coinText.text = coinScore.ToString();
-        modifierText.text = modifierScore.ToString();  }}
+        modifierText.text = modifierScore.ToString();
+  }
+}
