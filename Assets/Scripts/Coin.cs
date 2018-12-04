2c7b8ba1fbf65a6d884a7a34dd2ddfed437530a0
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-03 20:14:30 -0500

    Add coin prefab, make coin rotating and collected animations, WIP: coin script

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.Instance.GetCoin();
+            myAnimator.SetTrigger("Collected");
+        }
+    }
+}
