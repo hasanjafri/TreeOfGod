7653f78856c1c7027a33b8d8402d7c33d35d997d
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-05 01:48:49 -0500

    WIP: Polishing plane, prefabs and lighting

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -11,6 +11,7 @@ public class LevelManager : MonoBehaviour {
     //Level spawning mechanics
     private const float DISTANCE_BEFORE_SPAWN = 100.0f;
     private const int INITIAL_SEGMENTS = 10;
+    private const int INITIAL_TRANSITION_SEGMENTS = 2;
     private const int MAX_SEGMENTS_ON_SCREEN = 15;
     private Transform cameraContainer;
     private int amountOfActiveSegments;
@@ -47,7 +48,14 @@ public class LevelManager : MonoBehaviour {
         for (int i = 0; i < INITIAL_SEGMENTS; i++)
         {
             //Generate segment
-            GenerateSegment();
+            if (i < INITIAL_TRANSITION_SEGMENTS)
+            {
+                SpawnTransition();
+            }
+            else
+            {
+                GenerateSegment();
+            }
         }
     }
 
