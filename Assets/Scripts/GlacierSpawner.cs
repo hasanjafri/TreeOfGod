5e68d18198e8eccdca81f8617fdac6eb682ff8d7
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-06 17:33:11 -0500

    WIP: GlacierSpawner and scroller to produce parallax bg effect

---
+++ Assets/Scripts/GlacierSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlacierSpawner : MonoBehaviour {
+
+    private const float DISTANCE_TO_RESPAWN = 10.0f;
+
+    public float scrollSpeed = -2;
+    public float totalLength;
+    public bool IsScrolling { set; get; }
+
+    private float scrollLocation;
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (!IsScrolling)
+        {
+            return;
+        }
+
+        scrollLocation += scrollSpeed * Time.deltaTime;
+        Vector3 newLocation = (playerTransform.position.z + scrollLocation) * Vector3.forward;
+        transform.position = newLocation;
+    }
+}
